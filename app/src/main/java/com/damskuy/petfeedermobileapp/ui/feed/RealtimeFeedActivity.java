185715a30af25eb9df41f19685773530971cd9bf
Dave Russell <daverussell1309@gmail.com>
2023-04-20 18:47:34 +0700

    realtime feed input prompt

---
+++ app/src/main/java/com/damskuy/petfeedermobileapp/ui/feed/RealtimeFeedActivity.java
@@ -0,0 +1,17 @@
+package com.damskuy.petfeedermobileapp.ui.feed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.damskuy.petfeedermobileapp.R;
+
+public class RealtimeFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_realtime_feed);
+    }
+
+}
